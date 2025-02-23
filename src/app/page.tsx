baa735b0091d2ddb4dc62ad463737ad1171863c3
Kaweewaty <kaweewat.c@outlook.com>
2025-02-23 21:19:42 +0700

    กดตรงนี้ๆ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
   return (
     <div className={"flex flex-col items-center min-h-screen w-screen bg-white text-black overflow-hidden"}>
       {/* Timestamp and Status */}
-      <div className="text-center mt-[30vh] mb-12">
+      <div className="text-center mt-[20vh] mb-12">
         <p className="flex text-sm select-none">{currentTime ? currentTime : "Loading..."}</p>
         <div className="flex items-center justify-center gap-2">
           <span className="w-2 h-2 bg-green-500 rounded-full"></span>
@@ -85,13 +85,13 @@ export default function Home() {
         currentState == state.Queuing && (<>
           {/* Queue Number */}
           <h2 className="text-3xl font-semibold select-none">Your Queue</h2>
-          <p className="text-6xl font-bold mb-14 mt-4 select-none">{queueNumber !== null ? queueNumber : "Loading..."}</p>
+          <p className="text-6xl font-bold mb-14 mt-4 select-none font-kanit">{queueNumber !== null ? queueNumber : "กดหาพ่องงง"}</p>
 
           {/* Buttons */}
-          <button className="px-auto py-3 w-[12rem] font-bold border border-gray-300 bg-gray-300 text-gray-500 text-1xl rounded-full cursor-not-allowed select-none">
-            WAITING...
+          <button className="px-auto py-3 w-[12rem] font-bold border border-gray-300 bg-gray-300 text-gray-500 text-1xl rounded-full cursor-not-allowed font-kanit select-none">
+            หยอกน้าเตงๆ
           </button>
-          <button className="mt-3 mb-32 px-auto py-3 w-[12rem] font-bold border-[2px] border-black text-1xl rounded-full active:bg-black active:text-white select-none transition-all ease-out duration-100">
+          <button className="mt-3 mb-32 px-auto py-3 w-[12rem] font-bold border-[2px] border-black text-1xl rounded-full active:bg-black active:text-white select-none transition-all ease-out duration-100" onClick={() => setCurrentState(state.Home)}>
             CANCEL
           </button>
         </>)
